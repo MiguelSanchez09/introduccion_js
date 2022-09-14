846aaffea17f3a4c9b1efd195aa52bf440f6d21e
MiguelSanchez09 <miguelangelsanchez416@gmail.com>
2022-09-13 20:40:48 -0500

    introduccion_js

---
+++ main.js
@@ -0,0 +1,32 @@
+var Minombre = "Mi nombre es Miguel"
+
+console.log(Minombre)
+
+var Miapellido = "Mi apellido es Sanchez"
+
+console.log(Miapellido)
+
+var Miedad = 27
+
+console.log(Miedad)
+
+var desarrolladorweb = true
+
+console.log(desarrolladorweb)
+
+var today = new Date;
+var birthday = new Date ("1995, 6, 11");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
